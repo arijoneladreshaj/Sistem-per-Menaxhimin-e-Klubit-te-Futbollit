aa12ed6620b55d6afeb50026b57846f9474447ca
Verona <vk74423@ubt-uni.net>
2026-05-01 20:35:27 +0200

    LidhjaDB

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const sql = require("mssql");
+
+const config = {
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: 1433,
+  options: {
+    trustServerCertificate: true,
+    encrypt: false,
+  }
+};
+
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => { 
+    console.log("DB e lidhur!"); 
+    return pool; 
+  })
+  .catch(err => console.error("DB error:", err.message));
+
+module.exports = { sql, poolPromise };
